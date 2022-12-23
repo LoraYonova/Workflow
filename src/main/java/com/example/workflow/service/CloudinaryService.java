54a4274abcfca0e2d38d9273bfe72fdb273aff95
Лора Йонова <79514429+LoraYonova@users.noreply.github.com>
2022-12-23 23:10:12 +0200

    Implement CloudinaryService

---
+++ src/main/java/com/example/workflow/service/CloudinaryService.java
@@ -0,0 +1,13 @@
+package com.example.workflow.service;
+
+import com.example.workflow.service.impl.CloudinaryImage;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+public interface CloudinaryService {
+
+    CloudinaryImage upload(MultipartFile file) throws IOException;
+
+    boolean delete(String publicId);
+}
